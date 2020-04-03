a0ade6b259f2fc3bbd90f0daa6790a7057ebce31
liangjianmin <ljm@ichunt.com>
2020-04-03 17:29:56 +0800

    css

---
+++ js/pcb/pcb.js
@@ -0,0 +1,19 @@
+!function () {
+  window.pcbController = {
+    init: function () {
+      this.mounted(this).handleBind(this);
+    },
+    mounted: function (opt) {
+      
+      return this;
+    },
+    handleBind: function (opt) {
+      
+      
+      
+      return this;
+    }
+  }, $(function () {
+    pcbController.init();
+  })
+}();
