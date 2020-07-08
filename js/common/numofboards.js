40b2c0b424e14f68e4b68f57d4521e2150059177
shiyu <847476962@qq.com>
2020-07-08 17:02:07 +0800

    分离公共插件

---
+++ js/common/numofboards.js
@@ -0,0 +1,66 @@
+//数量选择插件
+;
+(function ($) {
+    $.fn.numofboards = function (callback) {
+        var parentDom = this;
+        var obj = {
+            init: function () {
+                if($(parentDom).find('.selects-qty').length){
+                    return
+                }
+                this.createDom.apply(parentDom)
+                this.bindFun.apply(parentDom)
+
+            },
+            createDom: function () {
+                var itemArr = [5, 10, 15, 20, 25, 30, 40, 50, 75, 100, 125, 150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000, 7500, 8000]
+                var str = '<div class="selects-qty">';
+                str += '<i class="lineBlock arrow"></i>';
+                str += '<dl class="clr">';
+                for (var i = 0; i < itemArr.length; i++) {
+                    str += ' <dd data-value="' + itemArr[i] + '"><i class="arr lineBlock va-m"></i><span class="lineBlock va-m">' + itemArr[i] + '</span></dd>'
+                }
+                str += '</dl><div class="wrap">';
+                str += '<label class="lineBlock va-m" style="width: 61px;">其他数量：</label>';
+                str += '<input type="number" class="inp va-m pcb_num_input" >';
+                str += '<div class="btn lineBlock va-m">';
+                str += '<a href="javascript:;" class="lineBlock ok">确认</a>';
+                str += '<a href="javascript:;" class="lineBlock canel">取消</a>';
+                str += '</div></div></div>';
+                $(this).append(str)
+            },
+            bindFun: function () {
+                var me = this;
+                //切换选择框下拉
+               $(document).find(me).find('.txt-wrap').on('click', function () {
+                    $(me).children('.selects-qty').toggle();
+                });
+                //切换选中
+                $(document).find(me).find('.selects-qty > dl > dd').on('click', function () {
+                    var val = $(this).attr('data-value');
+                    $(this).addClass('curr').siblings('dd').removeClass('curr');
+                    $(me).find('.txt-wrap input').val(val)
+                    $(me).find('.canel').trigger('click');
+                    if(callback){
+                        callback(val)
+                    }
+                    
+                  });
+                  //选择ok
+                  $(document).find(me).find('.ok').on('click', function () {
+                    var val = parseInt($.trim($(me).find(".pcb_num_input").val()));
+                    $(me).find('.txt-wrap input').val(val)
+                    $(me).find('.canel').trigger('click');
+                    if(callback){
+                        callback(val)
+                    }
+                  });
+                  //选中取消
+                  $(document).find(me).find('.canel').on('click', function () {
+                    $(me).find('.selects-qty').hide();
+                  });
+            }
+        }
+        obj.init()
+    };
+})(jQuery);
--- js/pcb/pcb.js
+++ js/pcb/pcb.js
@@ -1,42 +1,46 @@
-!function () {
+! function () {
   window.pcbController = {
     formObj: {},
     init: function () {
       this.mounted(this).handleBind(this).onscrollFn();
     },
     mounted: function (opt) {
-      
+
       $("html,body").animate({
         scrollTop: 118
-      },500)
-      
-      
+      }, 500)
+      $('#pcb_qty').numofboards(function (val) {
+        opt.formObj.pcb_num = val;
+        opt.calculation(opt);
+      })
+     
+
       return this;
     },
     handleBind: function (opt) {
-      
+
       //出货方式
       Util.tabs('#PcbUnit ul li', function (index, val) {
         //拼板方式 是否接受打叉板 代拼板参数
         if (index == 0) {
           $("#PanelWayContentParam").slideUp(); //代拼板参数
-          $("#separatingWay").slideUp();//分割方式
-          $("#pcb_acceptCrossedZone").slideUp();//是否接受打叉板
-          $("#PanelWayContent").slideUp();  //拼板方式
-          
+          $("#separatingWay").slideUp(); //分割方式
+          $("#pcb_acceptCrossedZone").slideUp(); //是否接受打叉板
+          $("#PanelWayContent").slideUp(); //拼板方式
+
         } else if (index == 1) {
           $("#PanelWayContentParam").slideUp(); //代拼板参数
-          
+
           $("#separatingWay").slideDown(); //分割方式
           $("#pcb_acceptCrossedZone").slideDown(); //是否接受打叉板
-          $("#PanelWayContent").slideDown();  //拼板方式
-          
+          $("#PanelWayContent").slideDown(); //拼板方式
+
         } else if (index == 2) {
-          $("#separatingWay").slideUp();   //分割方式
-          $("#PanelWayContent").slideUp();  //拼板方式
-          
-          $("#pcb_acceptCrossedZone").slideDown();  //是否接受打叉板
-          
+          $("#separatingWay").slideUp(); //分割方式
+          $("#PanelWayContent").slideUp(); //拼板方式
+
+          $("#pcb_acceptCrossedZone").slideDown(); //是否接受打叉板
+
           layer.open({
             type: 1,
             title: '<span>PCB工艺信息</span>',
@@ -46,111 +50,110 @@
             content: $("#pcbHtml").html(),
             success: function (layero, index) {
               layui.form.render();
-              
               $('input[name="pcb_num_layer"]').val($('input[name="pcb_num"]').val());
-              
+              $('#pcb_qty_layer').numofboards()
               opt.dataShow($("#widthNumberForModal").val(), $('#lengthNumberForModal').val() * 1, $("input[name='pcb_width']").val(), $("input[name='pcb_length']").val());
             }
           });
-          
-          
+
+
         }
       });
-      
+
       //水平，垂直槽间距
       $(document).on('input propertychange', '#spacingX', function () {
         var spacingX = $("#spacingX").val() * 1;
         var spacingY = $("#spacingY").val() * 1;
-        
+
         var X = $("#widthNumberForModal").val() * 1;
         var Y = $("#lengthNumberForModal").val() * 1;
-        
+
         if (spacingX) {
           opt.change(X, Y, spacingX, spacingY);
         }
       });
-      
+
       $(document).on('input propertychange', '#spacingY', function () {
         var spacingX = $("#spacingX").val() * 1;
         var spacingY = $("#spacingY").val() * 1;
-        
+
         var X = $("#widthNumberForModal").val() * 1;
         var Y = $("#lengthNumberForModal").val() * 1;
-        
+
         if (spacingY) {
           opt.change(X, Y, spacingX, spacingY);
         }
-        
-        
+
+
       });
-      
+
       //PCB工艺信息
       $(document).on('change', '#undis', function () {
         var value = $(this).val();
-        
+
         if (value == 0) {
           $(".needHold").addClass('needHoldnone');
-          
+
         } else if (value == 1) {
           $(".needHold").find('.left,.right').hide();
           $(".needHold").find('.bottom,.top').show();
-          
+
           $(".needHold").removeClass('needHoldnone');
           $(".needHold").removeClass('needHoldleft');
           $(".needHold").addClass('needHoldtop');
-          
+
         } else if (value == 2) {
           $(".needHold").find('.bottom,.top').hide();
           $(".needHold").find('.left,.right').show();
-          
+
           $(".needHold").removeClass('needHoldnone');
           $(".needHold").removeClass('needHoldtop');
           $(".needHold").addClass('needHoldleft');
-          
+
         } else if (value == 3) {
           $(".needHold").find('.left,.right').show();
           $(".needHold").find('.bottom,.top').show();
-          
+
           $(".needHold").removeClass('needHoldnone');
           $(".needHold").removeClass('needHoldleft');
           $(".needHold").removeClass('needHoldtop');
         }
       });
-      
-      
+
+
       $(document).on('input propertychange', '#widthNumberForModal', function () {
-        
-        
+
+
         var spacingX = $("#spacingX").val() * 1;
         var spacingY = $("#spacingY").val() * 1;
-        
+
         var X = $("#widthNumberForModal").val() * 1;
         var Y = $("#lengthNumberForModal").val() * 1;
-        
-        
+
+
         if (X) {
           opt.dataShow(X, $('#lengthNumberForModal').val() * 1, $("input[name='pcb_width']").val(), $("input[name='pcb_length']").val());
           opt.change(X, Y, spacingX, spacingY);
         }
-        
-        
+
+
       });
-      
+
       $(document).on('input propertychange', '#lengthNumberForModal', function () {
         var spacingX = $("#spacingX").val() * 1;
         var spacingY = $("#spacingY").val() * 1;
-        
+
         var X = $("#widthNumberForModal").val() * 1;
         var Y = $("#lengthNumberForModal").val() * 1;
-        
-        
+
+
         if (Y) {
           opt.dataShow($('#widthNumberForModal').val() * 1, Y, $("input[name='pcb_width']").val(), $("input[name='pcb_length']").val());
           opt.change(X, Y, spacingX, spacingY);
         }
       });
-      
-      
+
+
       //拼版规则
       Util.tabs('#pcb_pbtype ul li', function (index, val) {
         Observer.emit('pcb', {
@@ -159,7 +162,7 @@
           val: val
         });
       });
-      
+
       //是否接受打叉板
       Util.tabs('#pcb_acceptCrossedZone ul li', function (index, val) {
         Observer.emit('pcb', {
@@ -168,12 +171,12 @@
           val: val
         });
       });
-      
+
       //板子层数
       Util.tabs('#pcb_layer ul li', function (index, val) {
-        
+
         var layer = val;
-        
+
         //如果板子层数选1
         if (layer == 1) {
           Observer.emit('pcb', {
@@ -183,8 +186,8 @@
             val: val
           });
         }
-        
-        
+
+
         //如果板子层数选2层
         if (layer == 1 || layer == 2) {
           Observer.emit('pcb', {
@@ -193,8 +196,8 @@
             val: val
           });
         }
-        
-        
+
+
         //如果板子层数选4层
         if (layer == 4) {
           Observer.emit('pcb', {
@@ -204,7 +207,7 @@
             val: val
           });
         }
-        
+
         //如果板子层数选6层
         if (layer == 6) {
           Observer.emit('pcb', {
@@ -214,9 +217,9 @@
             val: val
           });
         }
-        
+
         //如果板子层数选8层
-        
+
         if (layer == 8 || layer == 10 || layer == 12) {
           Observer.emit('pcb', {
             type: 'pcb_layer',
@@ -225,32 +228,32 @@
             val: val
           });
         }
-        
-        
+
+
       });
-      
+
       //板材类型
       Util.tabs('#BoardType ul li', function (index, val) {
-      
+
       });
-      
+
       //FR4-TG
       Util.tabs('#FrTg ul li', function (index, val) {
-      
+
       });
-      
+
       //板子厚度
       Util.tabs('#pcb_thickness ul li', function (index, val) {
-        
+
         Observer.emit('pcb', {
           type: 'pcb_thickness',
           index: index,
           val: val
         });
-        
-        
+
+
       });
-      
+
       //铜箔厚度
       Util.tabs('#pcb_copperThickness ul li', function (index, val) {
         Observer.emit('pcb', {
@@ -259,12 +262,12 @@
           val: val
         });
       });
-      
+
       //最小线宽/线距
       Util.tabs('#LineWeight ul li', function (index, val) {
-      
+
       });
-      
+
       //最小孔径
       Util.tabs('#pcb_minholesize ul li', function (index, val) {
         Observer.emit('pcb', {
@@ -273,51 +276,51 @@
           val: val
         });
       });
-      
+
       //阻焊颜色
       Util.tabs('#SolderColor ul li', function (index, val) {
-      
+
       });
-      
+
       //字符颜色
       Util.tabs('#FontColor ul li', function (index, val) {
-      
+
       });
-      
+
       //焊盘喷镀
       Util.tabs('#SurfaceFinish ul li', function (index, val) {
-      
+
       });
-      
+
       //阻焊覆盖
       Util.tabs('#SolderCover ul li', function (index, val) {
-      
+
       });
-      
+
       //沉金厚度
       Util.tabs('#goldThickness ul li', function (index, val) {
-      
+
       });
-      
+
       //测试选项
       Util.tabs('#TestType ul li', function (index, val) {
-      
+
       });
-      
+
       //金(锡)手指
       Util.tabs('#Goldfinger ul li', function (index, val) {
-      
+
       });
-      
+
       //BGA数量
       Util.tabs('#BGANun ul li', function (index, val) {
-      
+
       });
-      
+
       //阻抗
       $(document).on('click', '#ImpedanceSize ul li', function () {
         var index = $(this).index();
-        
+
         if (index == 0) {
           $(this).addClass('curr').siblings('li').removeClass('curr');
           $("#impedancereportzone").hide();
@@ -333,63 +336,63 @@
             $(this).addClass('curr');
           }
         }
-        
+
       });
-      
-      
+
+
       //阻抗报告
       Util.tabs('#impedancereportzone ul li', function (index, val) {
-      
+
       });
-      
+
       //确认生产稿
       Util.tabs('#ProductionDraft ul li', function (index, val) {
-      
+
       });
-      
+
       //收缩
       $(document).on('click', '.shrink', function () {
         opt.onscrollFn();
         var statusDom = $(this).find('.arrow-icon');
-        
-        
+
+
         if (statusDom.hasClass('arrow-t')) {
-          
+
           statusDom.addClass('arrow-b').removeClass('arrow-t');
-          
-          
+
+
           if ($(this).attr('data-type') == 'param') {
             $(this).parents().parents().children('.detail').slideUp();
-            
+
           }
-          
+
           if ($(this).attr('data-type') == 'order') {
             $(this).parents().parents().find('.cost-wrap').slideUp();
           }
-          
+
         } else {
-          
+
           statusDom.addClass('arrow-t').removeClass('arrow-b');
-          
+
           if ($(this).attr('data-type') == 'param') {
             $(this).parents().parents().children('.detail').slideDown();
-            
+
             $('.cost-wrap').slideUp();
             $('.cost-wrap').parent().parent().find('.arrow-icon').addClass('arrow-b').removeClass('arrow-t');
-            
+
           }
-          
+
           if ($(this).attr('data-type') == 'order') {
             $(this).parents().parents().find('.cost-wrap').slideDown();
-            
+
             $(".detail").slideUp();
             $('.detail').parent().find('.arrow-icon').addClass('arrow-b').removeClass('arrow-t');
-            
+
           }
         }
       });
-      
-      
+
+
       $(document).on('click', '.board-up-layer .list-wrap li', function () {
         $(".board-up-layer .list-wrap li").find('.circle').removeClass('act');
         $(this).find('.circle').addClass('act');
@@ -400,76 +403,32 @@
       $(document).on('click', '.board-up-layer .canel', function () {
         layer.closeAll();
       });
-      
+
       //板子尺寸
       $("#BoardWidth").on('input propertychange', function () {
         var val = $(this).val();
         opt.formObj.BoardWidth = val;
-        
+
         opt.checkPcb(opt, opt.formObj);
       });
       $("#BoardHeight").on('input propertychange', function () {
         var val = $(this).val();
         opt.formObj.BoardHeight = val;
-        
+
         opt.checkPcb(opt, opt.formObj);
       });
-      
-      
-      //板子数量
-      $('#pcb_qty .txt-wrap').on('click', function () {
-        $("#pcb_qty").children('.selects-qty').toggle();
-      });
-      $("#pcb_qty").find('.selects-qty > dl > dd').on('click', function () {
-        var val = $(this).attr('data-value');
-        $(this).addClass('curr').siblings('dd').removeClass('curr');
-        $("#pcb_qty").find('.txt-wrap input').val(val)
-        $("#pcb_qty").find('.canel').trigger('click');
-        opt.formObj.pcb_num = val;
-        opt.calculation(opt);
-      });
-      $("#pcb_qty").find('.ok').on('click', function () {
-        var val = parseInt($.trim($("#pcb_num_input").val()));
-        $("#pcb_qty").find('.txt-wrap input').val(val)
-        $("#pcb_qty").find('.canel').trigger('click');
-        opt.formObj.pcb_num = val;
-        opt.calculation(opt);
-      });
-      $("#pcb_qty").find('.canel').on('click', function () {
-        $('#pcb_qty .selects-qty').hide();
-      });
-      
-      //弹窗板子数量
-      
-      $(document).on('click', "#pcb_qty_layer .txt-wrap", function () {
-        $("#pcb_qty_layer").children('.selects-qty').toggle();
-      });
-      
-      $(document).on('click', "#pcb_qty_layer .selects-qty > dl > dd", function () {
-        var val = $(this).attr('data-value');
-        $(this).addClass('curr').siblings('dd').removeClass('curr');
-        $("#pcb_qty_layer").find('.txt-wrap input').val(val);
-        $("#pcb_qty_layer").find('.canel').trigger('click');
-      });
-      
-      
-      $(document).on('click', "#pcb_qty_layer .ok", function () {
-        var val = parseInt($.trim($("#pcb_num_inputs").val()));
-        $("#pcb_qty_layer").find('.txt-wrap input').val(val);
-        $("#pcb_qty_layer").find('.canel').trigger('click');
-        
-      });
-      $(document).on('click', "#pcb_qty_layer .canel", function () {
-        $('#pcb_qty_layer .selects-qty').hide();
-      });
-      
-      
+
+
+
+  
+
+
       //监听函数
       Observer.on('pcb', function (e) {
-        
+
         //板子层数
         if (e.args.type == 'pcb_layer') {
-          
+
           //请提供多层板层压叠序
           if (e.args.index > 1) {
             layer.open({
@@ -490,80 +449,79 @@
                 $(layero).find('.input-group').empty().append(htmlArr.join(''));
               }
             });
-            
+
           }
-          
+
           //层数1
           if (e.args.val == 1) {
             Util.filter('#pcb_thickness ul li', [], true);
             Util.filter('#pcb_copperThickness ul li', e.args.copperThickness);
             $("#pcb_insidethickness").slideUp();
           }
-          
+
           //层数2
           if (e.args.val == 2) {
             Util.filter('#pcb_thickness ul li', [], true);
             Util.filter('#pcb_copperThickness ul li', [], true);
             $("#pcb_insidethickness").slideUp();
           }
-          
+
           //层数4
           if (e.args.val == 4) {
             Util.filter('#pcb_thickness ul li', e.args.thickness);
             Util.filter('#pcb_copperThickness ul li', [], true);
             $("#pcb_insidethickness").slideDown();
           }
-          
+
           //层数6
           if (e.args.val == 6) {
             Util.filter('#pcb_thickness ul li', e.args.thickness);
             Util.filter('#pcb_copperThickness ul li', [], true);
             $("#pcb_insidethickness").slideDown();
           }
-          
-          
+
+
         }
-        
+
         //收集表单数据
         opt.formObj[e.args.type] = e.args.val;
-        
+
         opt.calculation(opt);
       });
-      
-      
+
+
       return this;
     },
     checkPcb: function (opt, obj) {
-      
+
       var pcbwidth = obj.BoardWidth;
       var pcblength = obj.BoardHeight;
-      
+
       if (pcbwidth > 120 || pcblength > 120) {
         layer.msg('我司可生产最大长度为120cm,单位为cm,请重新输入!');
         return false;
       }
-      
-      
+
+
       opt.calculation(opt);
-      
-      
+
+
     },
     calculation: function (opt) {
-      
       if (this.formObj.BoardWidth && this.formObj.BoardHeight) {
         $("#showpcb .pcb_wl").text(this.formObj.BoardWidth + ' * ' + this.formObj.BoardHeight + 'cm');
       }
-      
+
       if (this.formObj.pcb_num) {
         $("#showpcb .pcb_num").text(this.formObj.pcb_num);
       }
-      
-      
+
+
       if (this.formObj.pcb_thickness) {
         $("#showpcb .pcb_thickness").text(this.formObj.pcb_thickness + 'mm');
       }
-      
-      
+
+
     },
     dataShow: function (width, length, widthVal, lengthVal) {
       var arrHtml = [];
@@ -574,29 +532,33 @@
         }
         arrHtml.push('</tr>');
       }
-      
+
       $(".datalist").empty().html(arrHtml.join(''));
-      
+
     },
     change: function (X, Y, spacingX, spacingY) {
       if (X >= 1 && Y >= 1) {
-        
+
         if (spacingX) {
           for (var i = 0; i < X - 1; i++) {
             for (var j = 0; j < Y; j++) {
-              $('.datalist').find('tr').eq(i).find('td').eq(j).css({borderBottom: "3px solid #0081D2"});
+              $('.datalist').find('tr').eq(i).find('td').eq(j).css({
+                borderBottom: "3px solid #0081D2"
+              });
             }
           }
         }
-        
+
         if (spacingY) {
           for (var i = 0; i < X; i++) {
             for (var j = 0; j < Y - 1; j++) {
-              $('.datalist').find('tr').eq(i).find('td').eq(j).css({borderRight: "3px solid #0081D2"});
+              $('.datalist').find('tr').eq(i).find('td').eq(j).css({
+                borderRight: "3px solid #0081D2"
+              });
             }
           }
         }
-        
+
       }
     },
     onscrollFn: function () {
